c4eca5067e7fff04d2c8d639ee61febbda0171a1
glaubervila <glauber.vila.verde@gmail.com>
2017-08-15 02:05:41 -0300

    Added travis.yml

---
+++ server/settings_travis.py
@@ -0,0 +1,19 @@
+import os
+
+print("Using Settings Travis")
+
+BASE_PROJECT = os.environ.get("TRAVIS_BUILD_DIR")
+print("BaseProject %s" % BASE_PROJECT)
+
+ALLOWED_HOSTS = '*'
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_PROJECT, 'db/travis.db'),
+    },
+}
