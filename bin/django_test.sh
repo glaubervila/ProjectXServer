c5de64cb79f9e9f146177c13fdf6b989074fd038
glaubervila <glauber.vila.verde@gmail.com>
2017-08-16 00:46:08 -0300

    Added travis.yml

---
+++ bin/django_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+
+echo "DJANGO MODULE ${DJANGO_SETTINGS_MODULE}"
+
+# Runing PEP8 recursive in all .py files
+#N_ERRORS=`find . -name \*.py -exec pep8 --exclude=migrations {} + | wc -l`
+N_ERRORS=`pep8 . | wc -l`
+
+if [ ${N_ERRORS} -lt 0 ]; then
+	echo -e "${GREEN}Success${NOCOLOR}: PEP8 not found errors"
+
+else
+    echo -e "${RED}ERROR${NOCOLOR}: PEP8 encountered ${N_ERRORS} errors."
+    pep8 .
+    exit 1
+fi
+
